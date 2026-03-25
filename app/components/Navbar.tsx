10b82d7998eac51ed38dc35e4ebdd94ca407354c
georgethepav <gpb.bdd@gmail.com>
2026-03-25 21:29:21 +0000

    Update Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -6,7 +6,7 @@ export default function Navbar() {
       <div className="max-w-6xl mx-auto flex justify-between items-center px-6 py-4">
 
         <div className="font-semibold text-lg">
-          George
+          George Billam
         </div>
 
         <div className="flex gap-6 text-sm text-zinc-400">
