6a4dcd84c4674e3d1a85504bcf826b45c43af515
georgethepav <gpb.bdd@gmail.com>
2026-03-25 22:46:10 +0000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-export const dynamic = "force-dynamic";
-
 "use client";
+
+export const dynamic = "force-dynamic";
 
 import Navbar from "./components/Navbar";
 import ProjectCard from "./components/ProjectCard";
