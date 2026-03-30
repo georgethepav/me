c237eba55da5bc2c90a9449c20cf1629f6903b84
georgethepav <gpb.bdd@gmail.com>
2026-03-30 19:38:37 +0100

    Update Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -10,9 +10,16 @@ export default function Navbar() {
         </div>
 
         <div className="flex gap-6 text-sm text-zinc-400">
-          <a href="#projects" className="hover:text-white">Projects</a>
-          <a href="#systems" className="hover:text-white">Digital Systems</a>
-          <a href="#ventures" className="hover:text-white">Other Ventures</a>
+          <a href="#projects" className="hover:text-white transition">Projects</a>
+          <a href="#systems" className="hover:text-white transition">Digital Systems</a>
+          <a href="#ventures" className="hover:text-white transition">Other Ventures</a>
+          <a href="#work-with-me" className="hover:text-white transition">Work With Me</a>
+          <a
+            href="#contact"
+            className="text-white border border-zinc-600 rounded-full px-4 py-1 hover:bg-white hover:text-black transition"
+          >
+            Contact
+          </a>
         </div>
 
       </div>
